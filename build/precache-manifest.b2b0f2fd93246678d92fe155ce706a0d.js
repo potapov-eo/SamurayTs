81c23b660c8760f3d8ae5219ef894fc894c44e47
potapov-eo <potapov.eo@yandex.ru>
2020-10-15 14:39:17 +0300

    36 task with bild

---
+++ build/precache-manifest.b2b0f2fd93246678d92fe155ce706a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3fa331d6b056e0ffeec9f90a61a127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b3f9a537a5cec1b8b3",
+    "url": "/static/css/main.a5849d3d.chunk.css"
+  },
+  {
+    "revision": "0c1b5d4cc3b4a7c36b9a",
+    "url": "/static/js/2.9243f137.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9243f137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b3f9a537a5cec1b8b3",
+    "url": "/static/js/main.2fe62755.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
